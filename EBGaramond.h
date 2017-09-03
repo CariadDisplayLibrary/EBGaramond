7202476de86caf678a929db9a59d7ef13f94a881
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 22:07:27 +0100

    Initial import

---
+++ EBGaramond.h
@@ -0,0 +1,39 @@
+#ifndef _EBGARAMOND_H
+#define _EBGARAMOND_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t EBGaramond10[];
+    extern const uint8_t EBGaramond11[];
+    extern const uint8_t EBGaramond12[];
+    extern const uint8_t EBGaramond13[];
+    extern const uint8_t EBGaramond14[];
+    extern const uint8_t EBGaramond15[];
+    extern const uint8_t EBGaramond16[];
+    extern const uint8_t EBGaramond17[];
+    extern const uint8_t EBGaramond18[];
+    extern const uint8_t EBGaramond19[];
+    extern const uint8_t EBGaramond20[];
+    extern const uint8_t EBGaramond21[];
+    extern const uint8_t EBGaramond22[];
+    extern const uint8_t EBGaramond23[];
+    extern const uint8_t EBGaramond24[];
+    extern const uint8_t EBGaramond25[];
+    extern const uint8_t EBGaramond26[];
+    extern const uint8_t EBGaramond27[];
+    extern const uint8_t EBGaramond28[];
+    extern const uint8_t EBGaramond29[];
+    extern const uint8_t EBGaramond30[];
+    extern const uint8_t EBGaramond31[];
+    extern const uint8_t EBGaramond32[];
+    extern const uint8_t EBGaramond33[];
+    extern const uint8_t EBGaramond34[];
+    extern const uint8_t EBGaramond35[];
+    extern const uint8_t EBGaramond36[];
+    extern const uint8_t EBGaramond37[];
+    extern const uint8_t EBGaramond38[];
+    extern const uint8_t EBGaramond39[];
+    extern const uint8_t EBGaramond40[];
+};
+#endif
